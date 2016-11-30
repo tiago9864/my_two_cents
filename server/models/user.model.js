2c2e31b307ebf30a5744525885aa80628aefd4e4
Justin Dennison <justin1dennison@gmail.com>
2016-11-30 18:49:20 -0500

    validpassword and set password methods on user model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,6 +1,7 @@
 
 var mongoose  = require('mongoose');
 var Schema = mongoose.Schema;
+var crypto = require('crypto'); //no npm install necessary because part of node js
 
 
 var userSchema = new Schema({
@@ -9,17 +10,31 @@ var userSchema = new Schema({
     required: true,
     unique: true
   },
-  password: {
-    type: String,
-    required: true
-  },
   age: {
     type: Number,
     min: 13,
     max: 120,
     required: true
+  },
+  hash: {
+    type: String
+  },
+  salt: {
+    type: String
   }
 });
+
+userSchema.methods.setPassword = function(password){
+  this.salt  = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64)
+                    .toString('hex');
+};
+userSchema.methods.validPassword = function(password){
+  var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64)
+                    .toString('hex');
+  return this.hash === hash; //if equal then you provided the correct password, otherwise NO!
+};
+userSchema.methods.generateJwt = function(){};
 
 var User = mongoose.model('User', userSchema);
 module.exports = User;
