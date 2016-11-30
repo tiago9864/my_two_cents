abb2695b8416ba74abca2f62f18ac54dbe90ba4b
Justin Dennison <justin1dennison@gmail.com>
2016-11-29 20:15:31 -0500

    so.... updating the summary kinda works on update of the body using the post hook

--- server/models/post.model.js
+++ server/models/post.model.js
@@ -31,5 +31,10 @@ postSchema.pre('findOneAndUpdate', function(){
   this.update({},{ $set: { postDate: new Date() } });
 });
 
+postSchema.post('findOneAndUpdate', function(post){
+  post.summary = post.body.slice(0, 100) + '...';
+  post.save();
+});
+
 var Post = mongoose.model('Post', postSchema);
 module.exports = Post;
