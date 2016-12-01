b0a272ea142e99791ca614eb18fb85eae98e36ab
Justin Dennison <justin1dennison@gmail.com>
2016-11-30 20:36:10 -0500

    implemented login route

--- server/routers/user.router.js
+++ server/routers/user.router.js
@@ -35,7 +35,34 @@ router.post('/users/signup', function(req, res){
     });
   });
 });
-router.post('/users/login', function(req, res){});
+router.post('/users/login', function(req, res){
+  //edge case missing information on the request
+  if(!req.body.email || !req.body.password){
+    return res.status(401).json({
+      msg: "The username or password you have provided is incorrect"
+    });
+  }
+  User.findOne({email: req.body.email}, function(err, user){
+    if(err){
+      return res.status(500).json({
+        msg: err
+      });
+    }
+    if(!user){
+      return res.status(401).json({
+        msg: "The username or password you have provided is incorrect"
+      });
+    }
+    if(!user.validPassword(req.body.password)){
+      return res.status(401).json({
+        msg: "The username or password you have provided is incorrect"
+      });
+    }
+    return res.status(200).json({
+      token: user.generateJwt()
+    });
+  });
+});
 router.put('/users/profile/:userId', function(req, res){});
 
 module.exports = router;
