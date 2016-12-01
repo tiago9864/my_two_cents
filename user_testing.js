5afae710546692eccba3643690a139305c7c04ae
Justin Dennison <justin1dennison@gmail.com>
2016-11-30 19:38:37 -0500

    implemented generateJWT function for user auth

---
+++ user_testing.js
@@ -0,0 +1,18 @@
+
+var User  = require('./server/models/user.model.js');
+
+//mock up a request object with a body
+var req = {};
+req.body = {
+  email: 'j@d.com',
+  age: 55,
+  password: 'password'
+};
+
+var user = new User(req.body);
+console.log(user);
+user.setPassword(req.body.password);
+console.log(user);
+var passwordIsValid = user.validPassword('password');
+console.log(passwordIsValid);
+console.log("JWT", user.generateJwt());
