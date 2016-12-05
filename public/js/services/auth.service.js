6e5105046547242033230c7d100f2b530bbf3fa1
Justin Dennison <justin1dennison@gmail.com>
2016-12-04 19:44:49 -0500

    added skeleton for the functions of the auth.service in angular

--- public/js/services/auth.service.js
+++ public/js/services/auth.service.js
@@ -5,6 +5,23 @@
   AuthService.$inject = [];
 
   function AuthService(){
-    
+
+    return {
+      currentUser: currentUser,
+      saveToken: saveToken,
+      getToken: getToken,
+      isLoggedIn: isLoggedIn,
+      signup: signup,
+      login: login,
+      logout: logout
+    }
+
+    function currentUser(){}
+    function saveToken(token){}
+    function getToken(){}
+    function isLoggedIn(){}
+    function signup(user){}
+    function login(user){}
+    function logout(){}
   }
 }());
