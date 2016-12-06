31a025a7ea83ad2aeff5d9563485e56d61ef7d34
Justin Dennison <justin1dennison@gmail.com>
2016-12-06 18:38:25 -0500

    implemented AuthService with our fingers crossed

--- public/js/services/auth.service.js
+++ public/js/services/auth.service.js
@@ -2,10 +2,10 @@
   angular.module('two-cents')
     .factory('AuthService', AuthService);
 
-  AuthService.$inject = [];
+  AuthService.$inject = ['$http', '$window'];
 
-  function AuthService(){
-
+  function AuthService($http, $window){
+    var localStorage = $window.localStorage;
     return {
       currentUser: currentUser,
       saveToken: saveToken,
@@ -14,14 +14,53 @@
       signup: signup,
       login: login,
       logout: logout
-    }
+    };
 
-    function currentUser(){}
-    function saveToken(token){}
-    function getToken(){}
-    function isLoggedIn(){}
-    function signup(user){}
-    function login(user){}
-    function logout(){}
+    function currentUser(){
+      // grabbing _id would be very similar to this
+      if(isLoggedIn()){
+        var token = getToken();
+        var payload = token.split('.')[1];
+        payload = $window.atob(payload);
+        payload = JSON.parse(payload);
+        return {
+          email: payload.email
+        };
+      } else {
+        return null;
+      }
+    }
+    function saveToken(token){
+      localStorage['two-cents-token'] = token;
+    }
+    function getToken(){
+      return localStorage['two-cents-token'];
+    }
+    function isLoggedIn(){
+      var token = getToken();
+      var payload;
+      if(token){
+        payload = token.split('.')[1];
+        payload = $window.atob(payload);
+        payload = JSON.parse(payload);
+
+        return payload.exp > Date.now() / 1000; //comparing the expiration date of our token to right now
+      } else {
+        return false;
+      }
+    }
+    function signup(user){
+      return $http.post('/users/signup', user);
+    }
+    function login(user){
+      return $http.post('/users/login', user)
+                  .then(function(response){
+                    var token = response.token;
+                    saveToken(token);
+                  });
+    }
+    function logout(){
+      localStorage.removeItem('two-cents-token');
+    }
   }
 }());
