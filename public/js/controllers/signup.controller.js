e019c8f1eb3fa5f67c1162bd8e8b5e85b3ed503a
Justin Dennison <justin1dennison@gmail.com>
2016-12-06 18:59:08 -0500

    created the signup view and wired up the signup functionality in order to register users

--- public/js/controllers/signup.controller.js
+++ public/js/controllers/signup.controller.js
@@ -2,9 +2,20 @@
   angular.module('two-cents')
     .controller('SignUpController', SignUpController);
 
-  SignUpController.$inject = [];
+  SignUpController.$inject = ['$scope', 'AuthService'];
 
-  function SignUpController(){
-    
+  function SignUpController($scope, AuthService){
+    $scope.signup = signup;
+
+    function signup(newUser){
+      AuthService.signup(newUser)
+          .then(function(){
+            alert('Yayyyy!!! Signing up!');
+          })
+          .catch(function(){
+            $scope.newUser = {};
+            alert('Your stuff is broked');
+          });
+    }
   }
 }());
