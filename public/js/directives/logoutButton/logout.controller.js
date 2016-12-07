70676884e4fef61faebd5627f96c704006115dcb
Justin Dennison <justin1dennison@gmail.com>
2016-12-07 18:32:47 -0500

    wired up logout functionality

--- public/js/directives/logoutButton/logout.controller.js
+++ public/js/directives/logoutButton/logout.controller.js
@@ -2,13 +2,15 @@
   angular.module('two-cents')
     .controller("LogoutController", LogoutController);
 
-  LogoutController.$inject = ['$scope'];
+  LogoutController.$inject = ['$scope', 'AuthService', '$location'];
 
-  function LogoutController($scope){
+  function LogoutController($scope, AuthService, $location){
     $scope.logout = logout;
 
     function logout(){
       console.log('Logging out!');
+      AuthService.logout();
+      $location.path('/login');
     }
   }
 }());
