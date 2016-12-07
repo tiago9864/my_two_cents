98de965a7554041a587b78faf9e32815f070539f
Justin Dennison <justin1dennison@gmail.com>
2016-12-06 19:49:41 -0500

    implemented login functionality

--- public/js/controllers/login.controller.js
+++ public/js/controllers/login.controller.js
@@ -2,9 +2,23 @@
   angular.module('two-cents')
     .controller("LoginController", LoginController);
 
-  LoginController.$inject = [];
+  LoginController.$inject = ['$scope', 'AuthService', '$location'];
 
-  function LoginController(){
-    
+  function LoginController($scope, AuthService, $location){
+    $scope.login = login;
+    $scope.required = true;
+
+    function login(user){
+      AuthService.login(user)
+        .then(function(){
+          $location.path('/');
+        })
+        .catch(function(){
+          //at this point you should communicate with the user about the error
+          // we are not going to in the sake of time
+          $scope.user = {};
+          $location.path('/login');
+        });
+    }
   }
 }());
